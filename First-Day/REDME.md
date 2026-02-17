9c4fc49ba906cb4a0824e81e004322e0f77e213b
sbshihab24 <sbshihab2000@example.com>
2026-02-18 00:28:27 +0600

    Run Hello World Program

---
+++ First-Day/REDME.md
@@ -0,0 +1,11 @@
+## Fast Api Hello world program
+
+#### Method (GET)
+
+**Topic**
+
+-venv creation 
+-Uvicorn setup
+- .gitignore 
+-Install FastAPI
+- FastAPI Hello World Program
--- First-Day/main.py
+++ First-Day/main.py
@@ -3,5 +3,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def aiquest():
-    return {"Django":"API"}
+
+
+
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/user")
+async def user():
+    return {"message": "User information",
+            "id": 12345,
+            "name": "Shihab",
+            "password": "secure123",
+            "phone": "0123456789",
+            "email": "shihab@example.com"
+            }
